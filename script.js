ee0b43807c678fde4fc2b959676e8da1bc8bebfd
Siddhant Gosavi <siddhantgosavi7@gmail.com>
2026-05-28 00:14:50 +0530

    nav bar

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ $(document).ready(function () {
     });
 
 });
+const messages = [  "Congratulations. You survived assignments that nobody will remember.",
+                    "Your GPA is temporary.",
+                    "“All this struggle… just to say ‘good morning sir’ in interviews.",
+                    "You came for education. You got PDFs.",
+                    "Achievement unlocked: Deadline Survivor.",
+                    "Your attendance increased. Happiness decreased.",
+                    "Behind every engineer is FINAL_FINAL_v2_REAL.",
+                    "Placed? No. Traumatized? Yes.",
+                    "You mastered writing programs you’ll never run again.",
+                    "You didn’t waste your college life. College wasted it for you.",
+                    "You’re not just a student. You’re a PDF collector.",
+                    "Your code compiles. Your life doesn’t.",
+                    "You survived the semester. Barely.",
+                    "Your GPA is a myth. Like Bigfoot.",
+                    "You’re not procrastinating. You’re just prioritizing panic.",
+                    "Your degree is a piece of paper. Your stress is real."
+                ];
+
+
+                let intervalStarted = false;
+
+                function startMessages() {
+                document.getElementById("msg").style.display = "block";
+                showMessage()
+                    if (!intervalStarted) {
+                        intervalStarted = true;
+                    }
+                }
+
+                function showMessage() {
+                    const random = messages[Math.floor(Math.random() * messages.length)];
+                    document.getElementById("msg").innerText = random;
+                }
+                
+                if (intervalStarted) {
+                    setInterval(showMessage, 3000);
+                }
+                
